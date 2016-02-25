29fa97c2cf503128f24e1cf2964078559788d9ef
zdanozdan <tomasz@mikran.pl>
2016-02-25 12:53:51 +0100

    remarketing

--- htdocs/themes/base/base_theme/foot.html.php
+++ htdocs/themes/base/base_theme/foot.html.php
@@ -27,22 +27,6 @@
    </div>
 <?php endif ?>
 
-<!-- Kod tagu remarketingowego Google -->
-<script type="text/javascript">
-/* <![CDATA[ */
-var google_conversion_id = 1060419300;
-var google_custom_params = window.google_tag_params;
-var google_remarketing_only = true;
-/* ]]> */
-</script>
-<script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
-</script>
-<noscript>
-<div style="display:inline;">
-<img height="1" width="1" style="border-style:none;" alt="" src="//googleads.g.doubleclick.net/pagead/viewthroughconversion/1060419300/?value=0&amp;guid=ON&amp;script=0"/>
-</div>
-</noscript>
-
 <script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
 </script>
 <script type="text/javascript">
--- htdocs/themes/base/base_theme/info.html.php
+++ htdocs/themes/base/base_theme/info.html.php
@@ -629,4 +629,20 @@ dynx_pagetype: 'offerdetail',
 dynx_totalvalue: <?php print $rec->data['price_brutto'] ?>,
 };
 </script>
+
+<!-- Kod tagu remarketingowego Google -->
+<script type="text/javascript">
+/* <![CDATA[ */
+var google_conversion_id = 1060419300;
+var google_custom_params = window.google_tag_params;
+var google_remarketing_only = true;
+/* ]]> */
+</script>
+<script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
+</script>
+<noscript>
+<div style="display:inline;">
+<img height="1" width="1" style="border-style:none;" alt="" src="//googleads.g.doubleclick.net/pagead/viewthroughconversion/1060419300/?value=0&amp;guid=ON&amp;script=0"/>
+</div>
+</noscript>
 	
